3038b9897b29380b153c6bc23293a485c51276cc
Anderson <anderflorez@gmail.com>
2017-02-13 01:22:02 -0500

    initial commit

---
+++ php/faces.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
+	<meta name="description" content="Faces">
+	<meta name="author" content="Anderson Florez">
+	<title>Faces</title>
+
+	<!-- Bootstrap Core CSS -->
+	<link rel="stylesheet" type="text/css" 
+		href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+		integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
+		crossorigin="anonymous">
+
+	<link rel="stylesheet" type="text/css" href="../css/faces.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-offset-1 col-sm-10">
+				<h1 class="text-center">Faces</h1>
+				<span id="menu-button" class="glyphicon glyphicon-menu-hamburger btn btn-default pull-right"></span>
+				<span class="clearfix"></span>
+			</div>
+			<div id="navigation" class="col-sm-offset-1 col-sm-10">
+				<ul class="nav nav-tabs nav-justified">
+					<li class="active"><a href="faces.php">Faces</a></li>
+					<li><a href="uploads.php">Upload</a></li>
+					<li><a href="search.php">Search</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-offset-1 col-sm-10">
+				
+			</div>
+		</div>
+	</div>
+	<!-- JQuery -->
+	<script type="text/javascript" 
+			src="https://code.jquery.com/jquery-3.1.1.min.js"
+			integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+			crossorigin="anonymous">
+	</script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script type="text/javascript"
+			src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
+			integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
+			crossorigin="anonymous">
+	</script>
+
+	<!-- Custome JavaScript -->
+	<script type="text/javascript" src="../js/faces.js"></script>
+</body>
+</html>
